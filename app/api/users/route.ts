e80111b378e158c800e6502c711ba90429ef8378
jonathan-kraus <jonathanckraus@gmail.com>
2025-12-07 01:13:28 -0500

    dd

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,8 +1,8 @@
 // app/api/users/route.ts
-import { prisma } from "@/lib/db";
+import db from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const users = await prisma.user.findMany();
+  const users = await db.user.findMany();
   return NextResponse.json(users);
 }
