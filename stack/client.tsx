04b4567b37863856719ccb79e0c51a4253443e79
jonathan-kraus <jonathanckraus@gmail.com>
2025-12-07 02:45:50 -0500

    ii

--- stack/client.tsx
+++ stack/client.tsx
@@ -1,6 +1,6 @@
 import { StackClientApp } from "@stackframe/stack";
 
 export const stackClientApp = new StackClientApp({
-  projectId: "project_e58ed77b-32d6-412e-9c56-1cb7e6ffd2a4", // from Stack dashboard
+  projectId: "e58ed77b-32d6-412e-9c56-1cb7e6ffd2a4", // from Stack dashboard
   tokenStore: "nextjs-cookie", // ✅ keep this
 });
