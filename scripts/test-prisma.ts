171614283dbe23d0cb51effa4d794d89a9c2437c
jonathan-kraus <jonathanckraus@gmail.com>
2025-12-07 23:32:00 -0500

    d5

--- scripts/test-prisma.ts
+++ scripts/test-prisma.ts
@@ -2,14 +2,18 @@
 import { PrismaClient } from "@/src/generated/client";
 import dotenv from "dotenv";
 import db from "../lib/db";
-import { createRequestId } from '@/lib/logger';
+import { createRequestId, createLogger } from '@/lib/logger';
+
 
 dotenv.config();
 export async function GET(request: Request) {
   // 1. Generate the Central Request ID
   const requestId = createRequestId();
   console.log(`Request ID: ${requestId}`);
-  const log = createLogger('API_Handler');
+  const log = createLogger('Prisma_Test_Script');
+
+log.info('Starting Prisma test script.', requestId);
+
   log.info(`Handling GET request with Request ID: ${requestId}`);
 async function main() {
   const rows = await db.jtemp.findMany();
@@ -25,10 +29,8 @@ main()
   })
   return new Response("Prisma test completed. Check console for output.");
 }
-import { createLogger } from '../lib/logger';
 
-const log = createLogger('Prisma_Test_Script');
 
-log.info('Starting Prisma test script.');
+
 
 
