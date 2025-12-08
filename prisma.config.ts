098f48f2ee4372822d2729bc429222851a7ad08c
jonathan-kraus <jonathanckraus@gmail.com>
2025-12-08 15:18:27 -0500

    f9

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,10 @@
 // prisma.config.ts
 import "dotenv/config";
-import db from "./lib/db";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: env("process.env.DATABASE_URL"),
-    },
+    url: env("DATABASE_URL"),
   },
-);
+});
