ace99c129470eb0ae7be18ddb7018d34e1bd3186
jonathan-kraus <jonathanckraus@gmail.com>
2025-12-09 18:49:49 -0500

    s1

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,17 +1,20 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/authOptions";
+"use client";
+import { useSession } from "next-auth/react";
 
-export default async function TestPage() {
-  const session = await getServerSession(authOptions);
+export default function TestPage() {
+  const { data: session } = useSession();
 
   return (
-    <div>
-      <h1>Server Component Test</h1>
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">Client Component Test</h1>
       {session ? (
-        <pre>{JSON.stringify(session.user, null, 2)}</pre>
+        <pre className="bg-gray-800 text-white p-4 rounded">
+          {JSON.stringify(session.user, null, 2)}
+        </pre>
       ) : (
         <p>No user logged in</p>
       )}
     </div>
   );
 }
+
