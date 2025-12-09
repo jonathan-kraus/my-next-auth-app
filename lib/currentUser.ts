843686668cca5eb334d8c61083c4c07ee80a0a7e
jonathan-kraus <jonathanckraus@gmail.com>
2025-12-08 21:11:11 -0500

    h5

--- lib/currentUser.ts
+++ lib/currentUser.ts
@@ -3,5 +3,5 @@ import { stackServerApp } from "@/stack/server";
 
 export async function getCurrentUser() {
   const user = await stackServerApp.getUser(); // uses server-side cookies/context
-  return user; // null if not logged in
+  return user; // null if not logged in.
 }
