04b36e73ece61e1da1339362bd03c5f9624218c6
jonathan-kraus <jonathanckraus@gmail.com>
2025-12-08 23:07:46 -0500

    i6

--- app/api/test-log/route.ts
+++ app/api/test-log/route.ts
@@ -1,12 +1,15 @@
 // app/api/test-log/route.ts
 import { NextResponse } from "next/server";
 import { dbFetch } from "@/lib/dbFetch";
+import { getCurrentUser } from "@/lib/currentUser";
 
 export async function GET(request: Request) {
   const TEST_NAME = `TestUser-${Date.now()}`;
   //const USER_ID = "70044dfe-d497-41d9-99ae-3d9e39761e6d"; // Melissa's id
   const USER_ID = "cmivgk9b2000004lgnewb1boe"; // Jonathan's id
   console.log(`--- STARTING JTEMP WRITE for ${TEST_NAME} ---`);
+  const user = getCurrentUser();
+  console.log("Current user:", user);
 console.log("About to write log with userId:", USER_ID);
   // 1. Write a log row
   await dbFetch(({ db }) =>
@@ -19,10 +22,12 @@ console.log("About to write log with userId:", USER_ID);
         requestId: null,
         metadata: {
           userAgent: request.headers.get("User-Agent") || "Unknown",
+          
           ip:
             request.headers.get("X-Forwarded-For") ||
             request.headers.get("Remote-Addr") ||
             "Unknown",
+            user: user as any,
         },
       },
     }),
