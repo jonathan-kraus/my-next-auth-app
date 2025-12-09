9d521c7fafe36fefaf2127fc227ee55f8b02b0ad
jonathan-kraus <jonathanckraus@gmail.com>
2025-12-08 21:06:30 -0500

    h4

--- lib/logs.ts
+++ lib/logs.ts
@@ -1,10 +1,10 @@
 // lib/logs.ts
-import { createLogger, createRequestId } from "@/lib/logger";
+//import { createLogger, createRequestId } from "@/lib/logger";
 import { dbFetch } from "./dbFetch";
 // const TEST_USER_ID = "cmivgk9b2000004lgnewb1boe";
 // const log = createLogger("Initiating_Log_Viewer_Page");
 // const requestId = createRequestId();
-console.log("logs module loaded");
+//console.log("logs module loaded");
 export async function getLogs(options?: { severity?: string }) {
   //await log.info(`Starting log viewer`, TEST_USER_ID, requestId);
     const logs = await dbFetch(({ db }) =>
