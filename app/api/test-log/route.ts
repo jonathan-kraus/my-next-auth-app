8c8d6fa70be046d61771e8f3906fd1cf78603b96
jonathan-kraus <jonathanckraus@gmail.com>
2025-12-08 23:08:30 -0500

    i7

--- app/api/test-log/route.ts
+++ app/api/test-log/route.ts
@@ -5,8 +5,8 @@ import { getCurrentUser } from "@/lib/currentUser";
 
 export async function GET(request: Request) {
   const TEST_NAME = `TestUser-${Date.now()}`;
-  //const USER_ID = "70044dfe-d497-41d9-99ae-3d9e39761e6d"; // Melissa's id
-  const USER_ID = "cmivgk9b2000004lgnewb1boe"; // Jonathan's id
+  const USER_ID = "70044dfe-d497-41d9-99ae-3d9e39761e6d"; // Melissa's id
+  //const USER_ID = "cmivgk9b2000004lgnewb1boe"; // Jonathan's id
   console.log(`--- STARTING JTEMP WRITE for ${TEST_NAME} ---`);
   const user = getCurrentUser();
   console.log("Current user:", user);
