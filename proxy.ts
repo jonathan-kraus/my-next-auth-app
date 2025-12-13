b1566ace731abe29fb2830875046b68337b01b6c
jonathan-kraus <jonathanckraus@gmail.com>
2025-12-13 16:08:18 -0500

    v12.1.23: proxy5

--- proxy.ts
+++ proxy.ts
@@ -1,11 +1,12 @@
-// middleware.ts
+// proxy.ts (root)
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request }); // uses NEXTAUTH_SECRET
+export async function proxy(request: NextRequest) {
+  const token = await getToken({ req: request });
 
   const { pathname } = request.nextUrl;
+
   const protectedRoutes = ["/notes"];
   const isProtectedRoute = protectedRoutes.some((route) =>
     pathname.startsWith(route),
