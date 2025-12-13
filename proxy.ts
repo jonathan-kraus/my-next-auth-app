26f85423cfe4a7c9f30ba4e80232e1db6d232fd3
jonathan-kraus <jonathanckraus@gmail.com>
2025-12-13 16:14:13 -0500

    v12.1.24: proxy6

--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,20 @@
 // proxy.ts (root)
 import { getToken } from "next-auth/jwt";
+import { appLog } from "./utils/app-log";
+import { createRequestId } from "./lib/uuidj";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function proxy(request: NextRequest) {
   const token = await getToken({ req: request });
 
   const { pathname } = request.nextUrl;
-
+  const requestId = createRequestId();
+  appLog({
+    source: "proxy",
+    message: "do we have token",
+    requestId,
+    metadata: { action: "mount", token: token, pathname: pathname },
+  });
   const protectedRoutes = ["/notes"];
   const isProtectedRoute = protectedRoutes.some((route) =>
     pathname.startsWith(route),
