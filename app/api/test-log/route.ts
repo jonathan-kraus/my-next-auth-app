ae8fd8d57f6e1ae8cd9da9e4cb6cdb90c491bb63
jonathan-kraus <jonathanckraus@gmail.com>
2025-12-23 22:43:26 -0500

    v12.04.30: v53

--- app/api/test-log/route.ts
+++ app/api/test-log/route.ts
@@ -1,14 +1,32 @@
-// app/api/test-log/route.ts
-import { NextRequest } from 'next/server';
-import { requireAuth } from '@/lib/requireAuth';
+import { auth } from '@/lib/auth';
+import { appLog } from '@/utils/app-log';
+import { createRequestId } from '@/lib/uuidj';
+import { timeStamp } from 'node:console';
 
-export async function GET(request: NextRequest) {
-  const { session, response } = await requireAuth();
-  if (!session) return response; // returns 401 automatically
-
-  // At this point, you’re guaranteed to have a signed‑in user
-  return Response.json({
-    message: 'You are signed in!',
-    user: session.user,
+export async function GET() {
+  const session = await auth();
+  const requestId = createRequestId();
+  if (!session) {
+    await appLog({
+      source: 'app/api/test-log/route.ts',
+      message: '---NO USER---',
+      requestId: requestId,
+      metadata: {
+        action: 'check',
+        timeStamp: timeStamp(),
+      },
+    });
+  }
+  await appLog({
+    source: 'app/api/test-log/route.ts',
+    message: '---We have a user---',
+    requestId: requestId,
+    metadata: {
+      action: 'check',
+      user: session?.user.email,
+      timeStamp: timeStamp(),
+    },
   });
+
+  return Response.json({ user: session?.user });
 }
